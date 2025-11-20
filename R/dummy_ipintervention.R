0560b350f9cc7ee6150f9523a4b78b7c4d28efb6
Samantha Yang <yangsam@smh.gemini-hpc.ca@geminilogin.unity.local>
2025-11-20 13:48:51 -0500

    check input add

--- R/dummy_ipintervention.R
+++ R/dummy_ipintervention.R
@@ -37,6 +37,8 @@
 #' dummy_ipintervention_mri_maid(nid = 1000, int_code = c("3AN40VA", "3SC40WC"))
 #' dummy_ipintervention_mri_maid(nid = 1000, int_code = "3SC40WC")
 #'
+#' @importFrom Rgemini check_input
+#'
 #' @export
 #'
 dummy_ipintervention_mri_maid <- function(
