c2cfb10a79d5751e4e081bc1788bc2d56582391a
Samantha Yang <yangsam@smh.gemini-hpc.ca@geminilogin.unity.local>
2025-11-28 19:57:48 -0500

    rda doc

---
+++ R/lookup_cci_mri_maid.R
@@ -0,0 +1,9 @@
+#' MRI CCI lookup table - MAID variant
+#'
+#' @format A data table with columns:
+#' \describe{
+#'   \item{intervention_code}{CCI intervention code}
+#'   \item{cci_short_title}{Short title}
+#'   \item{cci_long_title}{Long title}
+#' }
+"lookup_cci_mri_maid"
