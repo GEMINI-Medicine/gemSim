76ee3c2673e9269d4a61c9d3d98e575125621e14
Samantha Yang <yangsam@smh.gemini-hpc.ca@geminilogin.unity.local>
2025-12-09 11:41:15 -0500

    fix rda docs...

--- R/data.R
+++ R/data.R
@@ -1,35 +1,23 @@
-#' @title
-#' CCI lookup table for MAID and MRI codes
-#'
+#' @title CCI lookup table for MAID and MRI codes
 #' @description
 #' This is the section of the CCI intervention lookup table with codes for MRI and MAID.
 #' Its columns are :
 #' - `intervention_code` (`character`): CCI intervention code
 #' - `maid` (`logical`): TRUE if the code is a MAID code, FALSE otherwise
-#'
-#' @export
 #' @name lookup_cci
 #' @docType data
 NULL
 
-#' @title
-#' StatCan da21uid lookup for `dummy_locality` function
-#'
+#' @title StatCan dissemination area ID lookup
 #' @description
-#' It contains dissemination area IDs for Canada
-#'
-#' @export
+#' It contains dissemination area IDs for Canada, or da21uid.
 #' @name da21uid_statcan_v2021
 #' @docType data
 NULL
 
-#' @title
-#' Blood product lookup table for `dummy_transfusion` function
-#'
+#' @title Blood product lookup table for `dummy_transfusion` function.
 #' @description
-#' It contains OMOP codes for blood products
-#'
-#' @export
+#' It contains OMOP codes for blood products.
 #' @name blood_product_lookup
 #' @docType data
 NULL
