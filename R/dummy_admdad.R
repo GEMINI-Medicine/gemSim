6d35338c0e009a7fa1e9c1073610e62ca25285fe
Anne Loeffler <LofflerA@unityhealth.to>
2026-04-08 13:56:24 -0400

    use LOS distribution to determine admission time

--- R/dummy_admdad.R
+++ R/dummy_admdad.R
@@ -102,9 +102,9 @@ dummy_admdad <- function(nid = 1000,
   ############## CHECKS: for valid inputs: `n_id`, `n_hospitals`, `time_period`
   Rgemini:::check_input(list(nid, n_hospitals), "integer")
   Rgemini:::check_input(time_period, c("numeric", "character", "POSIXct"), length = 2)
-
+  
   time_period <- as.character(time_period)
-
+  
   # get start and end dates
   if (grepl("^[0-9]{4}$", time_period[1])) {
     # if the user only provided a year
@@ -121,7 +121,7 @@ dummy_admdad <- function(nid = 1000,
       }
     )
   }
-
+  
   if (grepl("^[0-9]{4}$", time_period[2])) {
     # if the user only provided a year
     end_date <- convert_dt(paste0(time_period[2], "-12-31"), "ymd")
@@ -137,70 +137,70 @@ dummy_admdad <- function(nid = 1000,
       }
     )
   }
-
+  
   # Make sure `nid` is at least `n_hospitals`
   if (nid < n_hospitals) {
     stop("Invalid user input.
     Number of encounters `nid` should at least be equal to `n_hospitals`")
   }
-
+  
   # set the seed if the input provided is not NULL
   if (!is.null(seed)) {
     set.seed(seed)
   }
-
+  
   ############### PREPARE OUTPUT TABLE ###############
   ## create all combinations of hospitals and dates
   hosp_names <- as.integer(seq(1, n_hospitals, 1))
   hosp_assignment <- sample(hosp_names, nid, replace = TRUE)
-
+  
   id_list <- 1:nid
   id_vector <- rep(id_list, times = rep(1, nid))
   site_vector <- rep(hosp_assignment, times = rep(1, nid))
-
+  
   data <- data.table(genc_id = id_vector, hospital_num = site_vector, stringsAsFactors = FALSE)
-
+  
   # turn year variable into actual date by randomly drawing date_time
   add_random_datetime <- function(n, start_date, end_date) {
     random_date <- as.Date(round(runif(n,
-      min = as.numeric(as.Date(start_date)),
-      max = as.numeric(as.Date(end_date))
+                                       min = as.numeric(as.Date(start_date)),
+                                       max = as.numeric(as.Date(end_date))
     )))
-
+    
     random_datetime <- format(as.POSIXct(random_date + dhours(sample_time_shifted(n,
-      xi = 19.5, omega = 6.29, alpha = 0.20, seed = seed
+                                                                                  xi = 19.5, omega = 6.29, alpha = 0.20, seed = seed
     )), tz = "UTC"), format = "%Y-%m-%d %H:%M")
-
+    
     return(random_datetime)
   }
-
+  
   # create discharge_date_time first (because data are pulled by discharge)
   data[, discharge_date_time := add_random_datetime(.N, start_date, end_date)]
-
+  
   ############### DEFINE VARIABLE DISTRIBUTIONS ###############
   ## AGE
   # create left-skewed distribution, truncated from 18-110
   age_distr <- function(nid = 10000, xi = 95, omega = 30, alpha = -10) {
     age <- rsn(nid, xi, omega, alpha)
-
+    
     # truncate at [18, 110]
     age <- as.integer(age[age >= 18 & age <= 110])
   }
-
-
+  
+  
   ############### ADD VARIABLES CLUSTERED BY HOSPITAL ###############
   # Any encounter characteristics (e.g., age/gender/discharge disposition) are
   # simulated as being clustered by hospital (i.e., each hospital will be
   # simulated as random intercept, i.e., different location parameter)
   add_vars <- function(hosp_data) {
     n_enc <- nrow(hosp_data)
-
+    
     ## AGE
     # create new age distribution for each hospital where location parameter xi
     # varies to create a random intercept by site
     age <- age_distr(xi = rnorm(1, 95, 5))
     hosp_data[, age := sample(age, n_enc, replace = TRUE)]
-
+    
     ## Gender (F/M/Other)
     prob <- data.table(
       "gender" = c("F", "M", "O"),
@@ -209,11 +209,11 @@ dummy_admdad <- function(nid = 1000,
     # Introduce random hospital-level variability
     prob[, p := t(rdirichlet(1, alpha = prob$p / 0.005))] # 0.005 = level of variability
     hosp_data[, gender := sample(prob$gender, n_enc,
-      replace = TRUE,
-      prob$p / sum(prob$p)
+                                 replace = TRUE,
+                                 prob$p / sum(prob$p)
     )]
     # make sure probs add up to 1 (see addition of constant above)
-
+    
     ## DISCHARGE DISPOSITION
     prob <- data.table(
       "discharge_disposition" = c(4, 5, 8, 9, 10, 20, 30, 40, 61, 62, 65, 66, 67, 72, 73, 74, 90),
@@ -224,27 +224,27 @@ dummy_admdad <- function(nid = 1000,
     ) # add small constant to Os to ensure it's not rounded to 0 below
     prob[, p := t(rdirichlet(1, alpha = prob$p / 0.005))] # 0.005 = level of hospital-level variability
     hosp_data[, discharge_disposition := as.integer(sample(prob$discharge_disposition, n_enc,
-      replace = TRUE, prob$p / sum(prob$p)
+                                                           replace = TRUE, prob$p / sum(prob$p)
     ))] # make sure probs add up to 1 (see addition of constant above)
-
+    
     ## Simulate LOS to derive discharge_date_time
     # create right-skewed distribution with randomly drawn offset by site]
     hosp_data[, los := {
       mean_hosp <- rnorm(1, mean = 1.27, sd = 0.11)
       rlnorm(.N, meanlog = mean_hosp, sdlog = 1.38)
     }, by = hospital_num] # hospital-level variation in distribution
-
+    
     hosp_data[, admission_date_time := format(
-      round_date(as.POSIXct(discharge_date_time, tz = "UTC") -
-        ddays(los), unit = "days"),
+      as.POSIXct(discharge_date_time, tz = "UTC") -
+        ddays(los),
       format = "%Y-%m-%d %H:%M", tz = "UTC"
     )]
-
+    
     # if `discharge_date_time` ends up before `admission_date_time`
     hosp_data[, los := as.numeric(difftime(ymd_hm(discharge_date_time), ymd_hm(admission_date_time), units = "days"))]
     hosp_data[los < 0, admission_date_time := format(ymd_hm(admission_date_time) - ddays(1), "%Y-%m-%d %H:%M")]
     # handle sampling edge case with negative los
-
+    
     ## Alternate level of care (ALC) & days spent in ALC
     # ALC flag
     prob <- data.table(
@@ -253,10 +253,10 @@ dummy_admdad <- function(nid = 1000,
     )
     prob[, p := t(rdirichlet(1, alpha = prob$p / 0.05))] # 0.05 = level of variability
     hosp_data[, alc_service_transfer_flag := sample(prob$alc_service_transfer_flag, n_enc,
-      replace = TRUE,
-      prob$p / sum(prob$p)
+                                                    replace = TRUE,
+                                                    prob$p / sum(prob$p)
     )] # make sure probs add up to 1 (see addition of constant above)
-
+    
     # Days spent in ALC (as integer)
     # If ALC = FALSE, ALC days are either coded as 0 or NA (random across sites)
     hosp_data[alc_service_transfer_flag == "FALSE", number_of_alc_days := sample(c(0, NA), 1, prob = c(.8, .2))]
@@ -274,7 +274,7 @@ dummy_admdad <- function(nid = 1000,
       ],
       alc_service_transfer_flag := NA
     ]
-
+    
     # randomly recode values referring to FALSE/TRUE to simulate real messiness of ALC coding
     coding <- t(
       data.table(
@@ -289,24 +289,24 @@ dummy_admdad <- function(nid = 1000,
       ) # this is intentional, some sites only code "true", everything else is missing...
     )
     code <- sample(seq_len(nrow(coding)), 1)
-
+    
     hosp_data[alc_service_transfer_flag == FALSE, alc_service_transfer_flag := coding[code, 1]]
     hosp_data[alc_service_transfer_flag == TRUE, alc_service_transfer_flag := coding[code, 2]]
     # code missing as NA or "" (randomly per site)
     hosp_data[is.na(alc_service_transfer_flag), alc_service_transfer_flag := sample(c(NA, ""), 1, prob = c(.8, .2))]
-
+    
     return(hosp_data)
   }
-
-
+  
+  
   # note: split data by hospital before running foverlaps to avoid working with massive tables
   cohort_hospitals <- split(data, data$hospital_num)
   data_all <- lapply(cohort_hospitals, add_vars)
-
-
+  
+  
   ##  Combine all
   data <- do.call(rbind, data_all)
-
+  
   ## Select relevant output variables
   data <- data[order(genc_id), .(
     genc_id,
@@ -319,9 +319,9 @@ dummy_admdad <- function(nid = 1000,
     alc_service_transfer_flag,
     number_of_alc_days
   )]
-
+  
   # Return as data.frame (instead of data.table) as this is what SQL queries return
   data <- as.data.frame(data)
-
+  
   return(data)
 }
